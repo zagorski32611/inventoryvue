02518632235748a78ee89277b6a1127f99a4a637
Joe <j.j.zagorski@gmail.com>
2018-11-23 18:06:46 -0500

    added javascript for index page

---
+++ app/javascript/packs/static_index.js
@@ -0,0 +1,34 @@
+import Vue from 'vue/dist/vue.esm'
+
+document.addEventListener ('DOMContentLoaded', () => {
+
+
+    var allProducts = new Vue({
+        el: "#all_products_button",
+        data: {
+            isShow: false,
+        }    
+    })
+
+    var addProducts = new Vue({
+        el: "#add_products_button",
+        data: {
+            isShow: false,
+        }    
+    })
+
+    var sellProducts = new Vue({
+        el: "#sell_products_button",
+        data: {
+            isShow: false,
+        }    
+    })
+
+
+    var newCustomer = new Vue({
+        el: "#new_customer_button",
+        data: {
+            isShow: false
+        }
+    })
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   root 'static_pages#index'
+
+  resources :static_pages
+  resources :products 
+  resources :customers
 end
